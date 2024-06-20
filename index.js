83b285a30f61ff9034d96d77ed9e0024c47a3734
ChetanDighole <digholechetan@gmail.com>
2024-06-21 01:14:07 +0530

    bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 const app = express()
 app.use(express.json())
